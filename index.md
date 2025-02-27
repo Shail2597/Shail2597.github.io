eaf20206c803d372353b956ebeccb18593bac48f
Shail Chaudhari <shail.ch259@gmail.com>
2025-02-26 23:45:16 -0600

    Evening 26 Feb

--- index.md
+++ index.md
@@ -1,3 +1,4 @@
 # CS30 Class-Work 
 - [Colour Swap Timer](Colour-Time-Swap)
 # CS30 Projects
+- [Interactive Scene](interactive-scene)
--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -7,10 +7,41 @@
 // - I have used the mouseWheel function to move the disks
 
 
+let towers = [[],[],[]];
+let numberOfDisks = 5;
+let colors, steps;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  initializeTowers();
+  generateDiscColors();
 }
 
 function draw() {
   background(220);
+  drawTowers();
 }
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function generateDiscColors(){
+  colors = [];
+  for (let i = 0; i < numberOfDisks; i++){
+    colors.push(color(random(255), random(255), random(255)));
+  }
+}
+
+function initializeTowers(){
+  towers = [[],[],[]];
+  for (let i = numberOfDisks; i > 0; i--){
+    towers[0].push(i);
+  }
+  steps = 0;
+}
+
+function drawTowers(){
+  let tower_width = width/3 - 50;
+  for (let i = 0; i < 3; i++) {
+    rect((i + 1) * tower_width, height / 3 - 100, 20, 200); // Draw each tower as a rectangle
+  }
