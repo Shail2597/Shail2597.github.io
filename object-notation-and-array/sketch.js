cb420cd76a3fd62589aa432529a9f07f315bf996
Shail Chaudhari <shail.ch259@gmail.com>
2025-03-13 12:11:42 -0600

    End of Class Mar 13
    Added functions to draw the containers, so initialize them and draw them.
    Also the change size with regard to the window size.
    
    Also made a new HTML page which will be my Start Page

--- object-notation-and-array/sketch.js
+++ object-notation-and-array/sketch.js
@@ -6,14 +6,54 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let colours = ["red", "green", "blue", "yellow", "purple", "orange", "pink", "brown"];
 let balls = {};
+let containerWidth, containerHeight, ball_diameter;
 let containers = {};
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  updateDimensions();
+  createContainers();
 }
 
 function draw() {
   background(220);
+  drawContainers();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  updateDimensions();
+  createContainers();
+}
+
+function createContainers() {
+  containers = {};
+  for (let i = 0; i < 7; i++) {
+    containers[i] = {
+      x: (i + 1) * width / 8,
+      y: height / 2,
+      width: containerWidth,
+      height: containerHeight,
+    };
+  }
+}
+
+function updateDimensions() {
+  containerWidth = width / 8;
+  containerHeight = height * 0.6;
+  ball_diameter = containerWidth;
+}
+
+
+function drawContainers(){
+  for (let key in containers) {
+    let container = containers[key];
+    fill(255);
+    stroke(0);
+    rectMode(CENTER);
+    rect(container.x, container.y, container.width, container.height);
+  }
 }
