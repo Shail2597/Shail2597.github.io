7aaee6224c070c440664894a032aacb01417ed24
Shail Chaudhari <shail.ch259@gmail.com>
2025-03-17 23:29:27 -0600

    End of Fay Mar  17
    So all change in this commit are done at home. Fixed the ball promlem and also completed the ball transfer mechnism So now the basic game is done only the add-ons left

--- object-notation-and-array/sketch.js
+++ object-notation-and-array/sketch.js
@@ -11,6 +11,8 @@ let colours = ["red", "green", "blue", "yellow"];
 let balls = {};
 let containerWidth, containerHeight, ball_diameter;
 let containers = {};
+let selectedBall = null;
+let selectedContainer = null;
 
 
 function setup() {
@@ -49,7 +51,6 @@ function updateDimensions() {
   containerWidth = width / 10;
   ball_diameter = containerWidth * 0.75;
   containerHeight =  ball_diameter*4;
-
 }
 
 
@@ -65,13 +66,16 @@ function drawContainers(){
 
 function createBall() {
   balls = {};
+  for (let i = 0; i < 4; i++) { // Ensure all 4 containers are initialized
+    balls[i] = []; // Initialize an empty array for each container
+  }
   for (let i = 0; i < 3; i++) {
-    balls[i] = [];
+    let shuffledColours = shuffle([...colours]); // Shuffle the colors for each container
     for (let j = 0; j < 4; j++) {
       balls[i].push({
         x: containers[i].x,
-        y:(height -  containers[i].y) + containers[i].height - ball_diameter/2,
-        colour: colours[j % colours.length],
+        y: containers[i].y + containerHeight / 2 - ball_diameter / 2 - (j * ball_diameter),
+        colour: shuffledColours[j % shuffledColours.length],
       });
     }
   }
@@ -84,5 +88,55 @@ function drawBalls() {
       noStroke();
       ellipse(ball.x, ball.y, ball_diameter);
     }
+  }
+}
+
+function mousePressed() {
+  if (selectedBall === null) {
+    // Select a ball
+    for (let containerIndex in balls) {
+      let containerBalls = balls[containerIndex];
+      if (containerBalls.length > 0) {
+        let topBall = containerBalls[containerBalls.length - 1]; // Get the topmost ball
+        let distance = dist(mouseX, mouseY, topBall.x, topBall.y);
+        if (distance < ball_diameter / 2) {
+          selectedBall = topBall;
+          selectedContainer = containerIndex;
+          balls[containerIndex].pop(); // Remove the ball from its container
+          return;
+        }
+      }
+    }
+  } else {
+    // Place the ball in a new container
+    for (let containerIndex in containers) {
+      let container = containers[containerIndex];
+      if (
+        mouseX > container.x - container.width / 2 &&
+        mouseX < container.x + container.width / 2 &&
+        mouseY > container.y - container.height / 2 &&
+        mouseY < container.y + container.height / 2
+      ) {
+        // Check if the container is not full
+        if (balls[containerIndex].length < 4) {
+          selectedBall.x = container.x;
+          selectedBall.y =
+            container.y +
+            containerHeight / 2 -
+            ball_diameter / 2 -
+            balls[containerIndex].length * ball_diameter;
+          balls[containerIndex].push(selectedBall); // Add the ball to the new container
+          selectedBall = null;
+          selectedContainer = null;
+          return;
+        } else {
+          console.log("Container is full!"); // Optional feedback
+        }
+      }
+    }
+    // If no valid container is clicked, return the ball to its original container
+    balls[selectedContainer].push(selectedBall);
+    selectedBall = null;
+    selectedContainer = null;
   }
 }
