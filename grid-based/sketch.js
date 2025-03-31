310cf9f8f53564c3e196a40db1e4a24260b06b58
Shail Chaudhari <shail.ch259@gmail.com>
2025-03-31 14:10:19 -0600

    End of Class Mar 31
    Solved the dual grid problems.
    A rough draft of the game is ready. All tiles open except for the mine tiles.

--- grid-based/sketch.js
+++ grid-based/sketch.js
@@ -1,11 +1,12 @@
 // Minesweeper
 // Shail Chaudhari
 // March 28, 2025
-//
+
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
 let cellSize;
-const SQUARE_DIMENSIONS = 10;
+const SQUARE_DIMENSIONS = 7;
 let topGrid, mineGrid;
 const OPEN_TILE = 0;
 const CLOSED_TILE = 1;
@@ -14,26 +15,23 @@ const MINE_TILE = 3;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  //make the largest square that fits
+  // Make the largest square that fits
   if (height > width) {
     cellSize = width / SQUARE_DIMENSIONS;
-  }
-  else {
+  } else {
     cellSize = height / SQUARE_DIMENSIONS;
   }
-  topGrid = generateGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
-  mineGrid = generateRandomGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
+  resetGame();
 }
 
 function draw() {
   background(220);
-
   displayGrid();
 }
 
 function keyPressed() {
   if (key === "r") {
-    topGrid = generateGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
+    resetGame();
   }
 }
 
@@ -43,12 +41,7 @@ function displayGrid() {
       if (topGrid[y][x] === CLOSED_TILE) {
         stroke(255);
         fill("black");
-      }
-      else if (topGrid[y][x] === OPEN_TILE) {
-        stroke(0);
-        fill("white");
-      }
-      if (mineGrid[y][x] === OPEN_TILE) {
+      } else if (topGrid[y][x] === OPEN_TILE) {
         stroke(0);
         fill("white");
       }
@@ -73,26 +66,35 @@ function generateRandomGrid(cols, rows) {
   for (let y = 0; y < rows; y++) {
     newGrid.push([]);
     for (let x = 0; x < cols; x++) {
-      if (random(100) < 50) {
-        newGrid[y].push(MINE_TILE);
+      newGrid[y].push(OPEN_TILE); // Initialize all cells as OPEN_TILE
+      if (random(100) < 20) { // Adjust the probability for mine placement
+        newGrid[y][x] = MINE_TILE; // Place a mine
       }
     }
   }
   return newGrid;
 }
 
-function mousePressed(){
+function mousePressed() {
   for (let y = 0; y < SQUARE_DIMENSIONS; y++) {
     for (let x = 0; x < SQUARE_DIMENSIONS; x++) {
-      if ( mouseX > x * cellSize && mouseX<x * cellSize + cellSize  && mouseY > y * cellSize && mouseY < y * cellSize + cellSize ) {
-        if (topGrid[y][x] === CLOSED_TILE){
-          if (mineGrid[y][x] === OPEN_TILE) {
-            topGrid[y][x] = OPEN_TILE ;
+      if (mouseX > x * cellSize && mouseX < x * cellSize + cellSize && mouseY > y * cellSize && mouseY < y * cellSize + cellSize) {
+        if (topGrid[y][x] === CLOSED_TILE) {
+          if (mineGrid[y][x] === MINE_TILE) {
+            // Game over logic
+            console.log("Game Over! You clicked on a mine.");
+            // Optionally, reveal all mines or stop the game
+          } else {
+            topGrid[y][x] = OPEN_TILE;
+            // Optionally, calculate and display adjacent mines here
           }
-          // else if (grid[y][x] === MINE_TILE) {
-          // }
         }
       }
     }
   }
+}
+
+function resetGame() {
+  topGrid = generateGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
+  mineGrid = generateRandomGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
 }
